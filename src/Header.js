743935f39d84c0d817bd3fd029048c17c94804f4
Ndéye Anta <ndeyeanta3.faye@ucad.edu.sn>
2026-04-10 10:43:12 +0000

    Setup initial SenTransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+    return(
+        <Header className="header">
+            <h1 className="header-titre">SenTransport</h1>
+            <p className="header-soustitre">
+                Votre guide du transport en commun a Dakar
+            </p>
+        </Header>
+    );
+}
+
+export default Header;
